440cd12445cb9dd29d8dfd73007da8a5d6e44560
3mpty <aurelien.corre@outlook.fr>
2025-07-12 11:44:18 +0200

    ui: Global ui setup.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Sidenav from "./components/sidenav/Sidenav";
+
+function App() {
+    return (
+        <main className="container">
+            <Sidenav />
+        </main>
+    );
+}
+
+export default App;
